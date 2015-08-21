8da790e067dd63cbe8e81d484949c4cce74af03a
HidetoSai <absolution09bloom@gmail.com>
2015-08-21 19:22:33 +0900

    no message

---
+++ Assets/Script/changetable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changetable : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public static float changeposition(float position) {
+		float temp;
+		float changed;
+
+		if (position <= 9 && position >= 1) {
+			temp = position - 5f;
+			changed = temp * -50f;
+			return changed; 
+		} else {
+			temp = position / -50f;
+			changed = temp + 5f;
+			return changed;
+		}
+	}
+
+	public static float BoardToWorld(int board_p) {
+		float changed;
+		changed = board_p * 50f;
+		return changed;
+	}
+}			
