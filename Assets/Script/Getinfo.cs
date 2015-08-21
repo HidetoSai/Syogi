1c0b38a5b172b8624719236277eba8fcfbc5c125
HidetoSai <absolution09bloom@gmail.com>
2015-08-21 19:17:41 +0900

    no message

--- Assets/Script/Getinfo.cs
+++ Assets/Script/Getinfo.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
 
 public class Getinfo : MonoBehaviour {
 
@@ -10,14 +12,20 @@ public class Getinfo : MonoBehaviour {
 	string url_pl = baseurl + pid;
 	string url_wi = baseurl + pid + "/winner";
 	string url_pi = baseurl + pid + "/pieces";
+	string[] statearray = new string[1];
+	string[,] usersarray = new string[2, 2];
+	string[] playarray = new string[4];
+	string[] winnerarray = new string[1];
+	string[,] piecearray = new string[40, 5];
+
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (Get (url_st));
-		StartCoroutine (Get (url_us));
-		StartCoroutine (Get (url_pl));
-		StartCoroutine (Get (url_wi));
-		StartCoroutine (Get (url_pi));
+		StartCoroutine (Get ((url_st), 1));
+		StartCoroutine (Get ((url_us), 2));
+		StartCoroutine (Get ((url_pl), 3));
+		StartCoroutine (Get ((url_wi), 4));
+		StartCoroutine (Get ((url_pi), 5));
 	}
 	
 	// Update is called once per frame
@@ -25,28 +33,93 @@ public class Getinfo : MonoBehaviour {
 	
 	}
 
-	IEnumerator Get(string url) {
+	IEnumerator Get(string url, int kindinfo) {
 		WWW www = new WWW (url);
 		yield return www;
-		if (www.error != null) {
-			Debug.Log ("Error");
-		} else {
-			Debug.Log (www.text);
+
+		var jsonData = Json.Deserialize(www.text) as Dictionary<string,object>;
+
+		switch (kindinfo) {
+		case 1:
+			state_parse(jsonData);
+			break;
+		case 2:
+			users_parse(jsonData);
+			break;
+		case 3:
+			play_parse(jsonData);
+			break;
+		case 4:
+			winner_parse(jsonData);
+			break;
+		case 5:
+			piece_parse(jsonData);
+			break;
+		default: 
+			break;
 		}
 	}
 
-	/*public WWW Get(string url) {
-		WWW www = new WWW (url);
-		StartCoroutine (WaitForRequest (www));
-		return www;
+	
+	void state_parse(Dictionary<string,object> json) {
+		int i = 0;
+		foreach(string Key in json.Keys) {
+			//Debug.Log ("json[" + Key + "]=" + json[Key]);
+			statearray[i] = json[Key].ToString();
+			Debug.Log(statearray[i]);
+		}
+	}
+	
+	void users_parse(Dictionary<string,object> json) {
+		int i = 0, j = 0;
+		foreach (string firstKey in json.Keys) {
+			//Debug.Log ("firstjson[" + firstKey + "]=" + json [firstKey]);
+			Dictionary<string,object> valueDic = (Dictionary<string, object>)json[firstKey];
+			foreach (string secondKey in valueDic.Keys) {
+				//Debug.Log ("secondjson[" + secondKey + "]=" + valueDic[secondKey]);
+				usersarray[i,j] = valueDic[secondKey].ToString();
+				Debug.Log(usersarray[i,j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	
+	void play_parse(Dictionary<string,object> json) {
+		int i = 0;
+		foreach(string Key in json.Keys) {
+			//Debug.Log ("json[" + Key + "]=" + json[Key]);
+			playarray[i] = json[Key].ToString();
+			Debug.Log (playarray[i]);
+			i++;
+		}
+		i = 0;
+	}
+	
+	void winner_parse(Dictionary<string,object> json) {
+		int i = 0;
+		foreach(string Key in json.Keys) {
+			//Debug.Log ("json[" + Key + "]=" + json[Key]);
+			if(json[Key] != null){
+				winnerarray[i] = json[Key].ToString();
+			}
+		}
+		i = 0;
 	}
 
-	private IEnumerator WaitForRequest(WWW www) {
-		yield return www;
-		if (www.error == null) {
-			Debug.Log("WWW Ok!: " + www.text);
-		} else {
-			Debug.Log("WWW Error: "+ www.error);
+	void piece_parse(Dictionary<string,object> json) {
+		int i = 0, j = 0;
+		foreach (string firstKey in json.Keys) {
+			//Debug.Log ("firstjson[" + firstKey + "]=" + json [firstKey]);
+			Dictionary<string,object> valueDic = (Dictionary<string, object>)json [firstKey];
+			foreach (string secondKey in valueDic.Keys) {
+				//Debug.Log ("secondjson[" + secondKey + "]=" + valueDic [secondKey]);
+				piecearray [i, j] = valueDic [secondKey].ToString ();
+				Debug.Log (piecearray [i, j]);
+				j++;
+			}
+			i++;
 		}
-	}*/
+	}
+
 }
